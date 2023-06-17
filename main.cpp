eb886f0e9e2249657f6a2b5c0abe0e93dcd927c7
wyatt-raex <hackleywyatt@gmail.com>
2023-06-17 14:39:43 -0400

    Setup -> Base Code - completed.

--- main.cpp
+++ main.cpp
@@ -1,30 +1,65 @@
+/*
+* Explinations on what things do what and what I understand about them will be
+* located in my Obsidian notes. 
+* - Reminder, try to keep comments in code brief, explicit, and clear. Add note 
+*   to which note file further explination is located.
+*/
+
 #include <vulkan/vulkan.h>
+
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
 
 #include <iostream>
 #include <stdexcept>
 #include <cstdlib>
 
+const uint32_t WIDTH = 800;
+const uint32_t HEIGHT = 600;
+
 class HelloTriangleApplication {
 public:
 	void run() {
+		initWindow();
 		initVulkan();
 		mainLoop();
 		cleanup();
 	}
 
+
 private:
+	GLFWwindow* window;
+
+
+	void initWindow() {
+		glfwInit();
+
+		glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API); // Not using OpenGL
+		glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE); // Don't allow resizing
+
+		window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
+	}
+
+
 	void initVulkan() {
 
 	}
 
-	void mainLoop() {
 
+	void mainLoop() {
+		while (!glfwWindowShouldClose(window)) {
+			glfwPollEvents();
+		}
 	}
+
 
 	void cleanup() {
+		glfwDestroyWindow(window);
 
+		glfwTerminate();
 	}
 };
+
 
 int main(int argc, char** argv) {
 	HelloTriangleApplication app;
